552006be47b00be4f4c7c831be3ecff644ef91a2
Delfos28 <delfos287@gmail.com>
2021-02-08 01:55:37 -0500

    first push

---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log ("Your index.js file is loaded correctly!");
+$("h3").hover(function(){
+    $(this).css("background-color", "orange");
+    }, function(){
+    $(this).css("background-color", "#ffffff");
+  });
